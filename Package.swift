d3c39e7d0cf5ba57cba42819d4ac03dabe87fe7d
Brennan-stack <55937591+Brennan-stack@users.noreply.github.com>
2024-08-29 11:55:04 -0400

    Update Wrapper to new SDK Format (#2)
    * updates to README to include info.plist requirements
    
    * Update SDK wrapper to new format

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "HurdlrSDK",
+    name: "Hurdlr",
     platforms: [
         .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "HurdlrSDK",
-            targets: ["HurdlrSDK"]),
+            name: "Hurdlr",
+            targets: ["Hurdlr"]),
     ],
     targets: [
         .binaryTarget(
-            name: "HurdlrSDK",
-            path: "HurdlrSDK.xcframework"
+            name: "Hurdlr",
+            path: "Hurdlr.xcframework"
         )
     ]
 )
