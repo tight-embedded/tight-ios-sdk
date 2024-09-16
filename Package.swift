3ce83b1144db9d0a20cd9853adf95a12923e1c37
Jared Nguyen <jtn2km@virginia.edu>
2024-09-16 11:09:06 -0400

    add wrapper target for binary dependencies

--- Package.swift
+++ Package.swift
@@ -6,18 +6,29 @@ import PackageDescription
 let package = Package(
     name: "Hurdlr",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Hurdlr",
-            targets: ["Hurdlr"]),
+            targets: ["HurdlrWrapperTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bugsnag/bugsnag-cocoa.git", exact: "6.30.1"),
     ],
     targets: [
         .binaryTarget(
             name: "Hurdlr",
             path: "Hurdlr.xcframework"
+        ),
+        .target(
+            name: "HurdlrWrapperTarget",
+            dependencies: [
+                .target(name: "Hurdlr"),
+                .product(name: "Bugsnag", package: "bugsnag-cocoa")
+            ],
+            path: "HurdlrWrapperTarget"
         )
     ]
 )
